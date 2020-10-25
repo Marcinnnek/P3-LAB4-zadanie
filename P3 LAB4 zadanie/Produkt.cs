de7bb0dcc458e6b3fba8e37b6474bf81bc57df91
Marcin Zuziak <porda1995@gmail.com>
2020-10-25 10:18:59 +0100

    struktura

---
+++ P3 LAB4 zadanie/Produkt.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P3_LAB4_zadanie
+{
+    class Produkt
+    {
+    }
+}
---
+++ P3 LAB4 zadanie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P3_LAB4_zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
